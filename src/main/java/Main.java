39eeef2bdf7b2579ee6d003b5cc3f0193023b3e6
Hongzhi Li <hongzhilig@gmail.com>
2023-10-20 16:30:01 -0600

    Docker push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,52 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
         System.out.println("welcome to the calculator");
         System.out.println("Enter a command:");
 
         Calculator calculator = new Calculator();
-        int number = 0;
+        int number;
+        boolean stop = false;
 
-        if (args[0].equals("add")){
-            number = calculator.add(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-            System.out.println(number);
-        }
-        else if (args[0].equals("subtract")) {
-            number = calculator.subtract(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-            System.out.println(number);
-        }
-        else if (args[0].equals("multiply")) {
-            number = calculator.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-            System.out.println(number);
-        }
-        else if (args[0].equals("divide")){
-            number = calculator.divide(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
-            System.out.println(number);
-        }
-        else if (args[0].equals("fibonacci")){
-            number = calculator.fibonacciNumberFinder(Integer.parseInt(args[1]));
-            System.out.println(number);
-        }
-        else if (args[0].equals("binary")){
-            String binarry;
-            binarry = calculator.intToBinaryNumber(Integer.parseInt(args[1]));
-            System.out.println(binarry);
+        while (!stop) {
+            Scanner object = new Scanner(System.in);
+            String command = object.next();
+            if(command.equals("stop")){
+                stop = true;
+            }
+            else if (command.equals("add")) {
+                int num1 = object.nextInt();
+                int num2 = object.nextInt();
+                number = calculator.add(num1, num2);
+                System.out.println(number);
+            } else if (command.equals("subtract")) {
+                int num1 = object.nextInt();
+                int num2 = object.nextInt();
+                number = calculator.subtract(num1, num2);
+                System.out.println(number);
+            } else if (command.equals("multiply")) {
+                int num1 = object.nextInt();
+                int num2 = object.nextInt();
+                number = calculator.multiply(num1, num2);
+                System.out.println(number);
+            } else if (command.equals("divide")) {
+                int num1 = object.nextInt();
+                int num2 = object.nextInt();
+                number = calculator.divide(num1, num2);
+                System.out.println(number);
+            } else if (command.equals("fibonacci")) {
+                int num1 = object.nextInt();
+                number = calculator.fibonacciNumberFinder(num1);
+                System.out.println(number);
+            } else if (command.equals("binary")) {
+                int num1 = object.nextInt();
+                String binarry;
+                binarry = calculator.intToBinaryNumber(num1);
+                System.out.println(binarry);
+            } else {
+                System.out.println("NOOOOOO");
+            }
         }
     }
 }
