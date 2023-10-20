230c6a3729facbf3a148ee814727d392bb507fdd
Hongzhi Li <hongzhilig@gmail.com>
2023-10-20 15:39:18 -0600

    Docker push

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,35 @@
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Testing Dockerfile");
+        System.out.println("welcome to the calculator");
+        System.out.println("Enter a command:");
+
+        Calculator calculator = new Calculator();
+        int number = 0;
+
+        if (args[0].equals("add")){
+            number = calculator.add(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+            System.out.println(number);
+        }
+        else if (args[0].equals("subtract")) {
+            number = calculator.subtract(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+            System.out.println(number);
+        }
+        else if (args[0].equals("multiply")) {
+            number = calculator.multiply(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+            System.out.println(number);
+        }
+        else if (args[0].equals("divide")){
+            number = calculator.divide(Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+            System.out.println(number);
+        }
+        else if (args[0].equals("fibonacci")){
+            number = calculator.fibonacciNumberFinder(Integer.parseInt(args[1]));
+            System.out.println(number);
+        }
+        else if (args[0].equals("binary")){
+            String binarry;
+            binarry = calculator.intToBinaryNumber(Integer.parseInt(args[1]));
+            System.out.println(binarry);
+        }
     }
 }
